91d816a7ccc141c76fa89cd9e17fb7993eff62b1
Paul Bohm <bohmps@gmail.com>
2011-07-21 18:35:00 -0700

    build changes

--- build-gvpe.sh
+++ build-gvpe.sh
@@ -6,7 +6,9 @@ if [ ! -e ./build/src/gvpectrl ]; then
     rm -rf libev
     cp -R ../../libev libev
     ACLOCAL=aclocal AUTOMAKE=automake ./autogen.sh --enable-hmac-length=16 --enable-rand-length=8 --enable-digest=sha1 --prefix="" --exec-prefix=""
-    cd src
+    cd lib
+    make
+    cd ../src
     make
     cd ../..
 fi

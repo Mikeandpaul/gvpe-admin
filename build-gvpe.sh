fe6109bfa0cfeb1c64430449cd9fe18932a4eca6
Paul Bohm <bohmps@gmail.com>
2011-08-11 22:19:26 -0700

    phone convo

--- build-gvpe.sh
+++ build-gvpe.sh
@@ -5,7 +5,8 @@ if [ ! -e ./build/src/gvpectrl ]; then
     cd build
     rm -rf libev
     cp -R ../../libev libev
-    ACLOCAL=aclocal AUTOMAKE=automake ./autogen.sh --enable-hmac-length=16 --enable-rand-length=8 --enable-digest=sha1 --prefix="" --exec-prefix=""
+    #ACLOCAL=aclocal AUTOMAKE=automake ./autogen.sh --enable-hmac-length=16 --enable-rand-length=8 --enable-digest=sha1 --prefix="" --exec-prefix=""
+    ACLOCAL=aclocal AUTOMAKE=automake ./autogen.sh --enable-cipher=bf --enable-digest=md4 --prefix="" --exec-prefix=""
     cd lib
     make
     cd ../src
